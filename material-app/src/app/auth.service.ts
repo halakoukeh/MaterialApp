b1482deca8f37ae40324f123fcec9ac6a339d898
Hala Koukeh <hala@homebazar.com>
2018-09-23 13:03:13 +0400

    tutorial stopped at authenticating via auth0

--- material-app/src/app/auth.service.ts
+++ material-app/src/app/auth.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {Router} from '@angular/router';
+import * as auth0 from 'auth0-js';
 
+(window as any).global = window;
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  autho0 = new auth0.WebAuth({
+    clientID: 'c8bhJzJhhYsqaFyC7Riy9SrP0aM8vqwa',
+    domain: 'halakouka@auth0.com',
+    responseType: 'token id_token',
+    redirectUri: 'http://localhost:4200',
+    scope: 'openid'
+  });
 
+  public login(): void {
+    this.autho0.authorize();
+  }
+  
   constructor() { }
 }
